d50eb811cf789c428f2fef4716d2ac3b13d556ad
Mark Tucker <mtucker@sidefx.com>
2020-07-22 03:39:01 -0400

    Add checks for the validity of the USD prim being synced before passing the prim to TrackVariability or UpdateForTime.

--- pxr/usdImaging/usdImaging/delegate.cpp
+++ pxr/usdImaging/usdImaging/delegate.cpp
@@ -343,7 +343,8 @@ public:
             SdfPath const& cachePath = _tasks[i];
 
             _HdPrimInfo *primInfo = _delegate->_GetHdPrimInfo(cachePath);
-            if (TF_VERIFY(primInfo, "%s\n", cachePath.GetText())) {
+            if (TF_VERIFY(primInfo, "%s\n", cachePath.GetText()) &&
+                primInfo->usdPrim) {
                 UsdImagingPrimAdapterSharedPtr const& adapter=primInfo->adapter;
                 if (TF_VERIFY(adapter, "%s\n", cachePath.GetText())) {
                     adapter->TrackVariability(primInfo->usdPrim,
@@ -368,7 +369,8 @@ public:
             SdfPath const& cachePath = _tasks[i];
 
             _HdPrimInfo *primInfo = _delegate->_GetHdPrimInfo(cachePath);
-            if (TF_VERIFY(primInfo, "%s\n", cachePath.GetText())) {
+            if (TF_VERIFY(primInfo, "%s\n", cachePath.GetText()) &&
+                primInfo->usdPrim) {
                 UsdImagingPrimAdapterSharedPtr const& adapter=primInfo->adapter;
                 if (TF_VERIFY(adapter, "%s\n", cachePath.GetText())) {
                     adapter->UpdateForTime(primInfo->usdPrim,
