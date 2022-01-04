928c584e879966eac6f9b5dfcc7a5cc6014553dd
Mark Tucker <mtucker@sidefx.com>
2022-01-04 16:03:01 -0500

    Add new HdSceneDelegate and HdPrim methods to get a "data sharing id" for an HdPrim. The purpose of this id (an SdfPath) is to provide an identifier that can be used to share data between multiple instancer prototypes when these prototypes in USD all share a common prototype primitive, but where hydra has created multiple HdPrims (since hydra creates a separate HdPrim for every prototype of every point instancer, even if multiple point instancers use identical instanceable prims as their prototypes).
    Ideally this would be unnecessary if a single prototype HdPrim could be shared
    by multiple HdInstancers, but that is not currently possible. This sharing id
    allows render delegates to build this kind of functionality for themselves.

--- pxr/usdImaging/usdImaging/delegate.h
+++ pxr/usdImaging/usdImaging/delegate.h
@@ -416,6 +416,9 @@ public:
                      int instanceIndex,
                      HdInstancerContext *instancerContext = nullptr) override;
 
+    USDIMAGING_API
+    virtual SdfPath GetDataSharingId(SdfPath const& primId) override;
+
     // ExtComputation support
     USDIMAGING_API
     TfTokenVector
