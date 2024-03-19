083ab0c0b7baa653846866e97f174f71c5f1f3b2
Rob Pieké <robp@sidefx.com>
2024-03-19 16:05:22 +0000

    Pick up Houdini Qt plugins and fonts

--- pxr/usdImaging/bin/usdview/usdview.py
+++ pxr/usdImaging/bin/usdview/usdview.py
@@ -27,6 +27,27 @@ from __future__ import print_function
 import sys
 import pxr.Usdviewq as Usdviewq
 
+from PySide2 import QtCore
+import os
+
+libpaths = list(QtCore.QCoreApplication.libraryPaths())
+# Get the dso path on Linux and Mac, or HB on Windows.
+if os.name == 'nt':
+    libpath = os.environ.get('HB')
+else:
+    libpath = os.environ.get('HDSO')
+
+# Add the Houdini Qt_plugins directory so we find the plugin for the
+# current platform.
+if libpath is not None:
+    libpaths.append(libpath + '/Qt_plugins')
+    QtCore.QCoreApplication.setLibraryPaths(libpaths)
+
+# Register the Houdini fonts directory with Qt.
+HFS = os.environ.get("HFS")
+if HFS is not None:
+    os.environ["QT_QPA_FONTDIR"] = "{}/houdini/fonts".format(HFS)
+
 if __name__ == '__main__':
     # Let Ctrl-C kill the app.
     import signal
