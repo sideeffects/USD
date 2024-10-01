f21137ce5277989a5c3a151b09518e382e11d587
Rob Pieké <robp@sidefx.com>
2024-10-01 15:09:08 +0100

    Moving logic for iterating over render delegate's material contexts from the scene delegate to the material schema. This ensures that the fallback context is only used if *all* the delegate's contexts fail.
    Related to https://github.com/PixarAnimationStudios/OpenUSD/issues/3286

--- pxr/imaging/hd/materialSchema.h
+++ pxr/imaging/hd/materialSchema.h
@@ -69,7 +69,7 @@ public:
     HdMaterialNetworkSchema GetMaterialNetwork();
 
     HD_API
-    HdMaterialNetworkSchema GetMaterialNetwork(TfToken const &context);
+    HdMaterialNetworkSchema GetMaterialNetwork(TfTokenVector const &contexts);
 
 // --(END CUSTOM CODE: Schema Methods)--
 
