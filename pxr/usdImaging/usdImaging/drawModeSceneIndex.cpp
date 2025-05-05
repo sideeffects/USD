81845879c1eda6fca9e2d1021c24f4d5336b9457
unhyperbolic <unhyperbolic@users.noreply.github.com>
2025-05-05 16:37:34 -0700

    UsdImagingDrawModeSceneIndex: It no longer looks whether something is a native instance - and thus replaces a native instance with a draw mode stand-in if the native instance says so.
    (Internal change: 2365983)

--- pxr/usdImaging/usdImaging/drawModeSceneIndex.cpp
+++ pxr/usdImaging/usdImaging/drawModeSceneIndex.cpp
@@ -8,7 +8,6 @@
 
 #include "pxr/usdImaging/usdImaging/drawModeStandin.h"
 #include "pxr/usdImaging/usdImaging/geomModelSchema.h"
-#include "pxr/usdImaging/usdImaging/usdPrimInfoSchema.h"
 
 #include "pxr/usd/sdf/path.h"
 
@@ -19,19 +18,6 @@ PXR_NAMESPACE_OPEN_SCOPE
 namespace
 {
 
-bool
-_IsUsdNativeInstance(const HdSceneIndexPrim &prim)
-{
-    UsdImagingUsdPrimInfoSchema primInfoSchema =
-        UsdImagingUsdPrimInfoSchema::GetFromParent(prim.dataSource);
-
-    HdPathDataSourceHandle const ds = primInfoSchema.GetNiPrototypePath();
-    if (!ds) {
-        return false;
-    }
-    return !ds->GetTypedValue(0.0f).IsEmpty();
-}
-
 // Resolve draw mode for prim from input scene index.
 // Default draw mode can be expressed by either the empty token
 // or UsdGeomTokens->default_.
@@ -39,15 +25,6 @@ TfToken
 _GetDrawMode(const HdSceneIndexPrim &prim)
 {
     static const TfToken empty;
-
-    if (_IsUsdNativeInstance(prim)) {
-        // Do not apply draw mode to native instance.
-        // Instead, the native instance prototype propagating scene index
-        // will create a copy of the prototype with the apply draw mode set
-        // and the draw mode scene index processing that prototype applies
-        // the draw mode.
-        return empty;
-    }
 
     UsdImagingGeomModelSchema geomModelSchema =
         UsdImagingGeomModelSchema::GetFromParent(prim.dataSource);
