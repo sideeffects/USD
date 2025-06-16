387609acf08e7949a127cfbecb55bb6b3214d309
blevin <blevin@users.noreply.github.com>
2025-06-15 18:01:12 -0700

    [usdImaging] niInstanceAggregationSceneIndex now updates its internal instance-tracking data structures prior to sending notification to observers.  This ensures that observers will see the correct result if they query instancer topology.
    (Internal change: 2370064)

--- pxr/usdImaging/usdImaging/niInstanceAggregationSceneIndex.cpp
+++ pxr/usdImaging/usdImaging/niInstanceAggregationSceneIndex.cpp
@@ -1277,6 +1277,9 @@ _InstanceObserver::_AddInstance(const SdfPath &primPath,
     const SdfPath instancerPath =
         info.GetInstancerPath();
 
+    // Update this map prior to sending notification below.
+    _instanceToInfo[primPath] = info;
+
     std::shared_ptr<SdfPathSet> &instances =
         prototypeNameToInstances[info.prototypeName];
     if (instances) {
@@ -1285,10 +1288,16 @@ _InstanceObserver::_AddInstance(const SdfPath &primPath,
                 HdInstancerTopologySchemaTokens->instanceIndices),
             HdPrimvarsSchema::GetDefaultLocator()};
 
+        // Update instances list prior to sending notification.
+        instances->insert(primPath);
+
         _retainedSceneIndex->DirtyPrims(
             { { instancerPath, locators } });
     } else {
         instances = std::make_shared<SdfPathSet>();
+
+        // Update instances list prior to sending notification.
+        instances->insert(primPath);
 
         _retainedSceneIndex->AddPrims(
             { // Add propagated prototype base prim
@@ -1305,10 +1314,6 @@ _InstanceObserver::_AddInstance(const SdfPath &primPath,
                     instances,
                     _forNativePrototype) } });
     }
-
-    instances->insert(primPath);
-
-    _instanceToInfo[primPath] = info;
 
     // Add (lazy) instance data source to instance.
     _retainedSceneIndex->AddPrims(
