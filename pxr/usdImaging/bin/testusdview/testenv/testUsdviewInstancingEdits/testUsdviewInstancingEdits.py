7ce2ba931d6e09706f4b696ccd666ac2c71e3ebe
mrawde <mrawde@users.noreply.github.com>
2026-05-15 10:10:25 -0700

    [usdImaging] Handle edge case of added prim going from "over" to "not over"
    (Internal change: 2409917)
    (Internal change: 2409924)
    (Internal change: 2410049)

--- pxr/usdImaging/bin/testusdview/testenv/testUsdviewInstancingEdits/testUsdviewInstancingEdits.py
+++ pxr/usdImaging/bin/testusdview/testenv/testUsdviewInstancingEdits/testUsdviewInstancingEdits.py
@@ -39,7 +39,37 @@ def _testInstancingEdits6146(appController):
 
     # If we get this far without crashing, we're good for now.
 
+# Tests that when a prim goes from being a "prototype" to no longer, we image
+# it properly.
+# 
+# Since we will sometimes use a type-less "over" to store prototypes, that is
+# included here.
+def _testMakePrototypeNotAPrototype(appController):
+    from pxr import Sdf
+
+    # The scene has 4 cubes:
+    # "left", "Over", "PointInstancer", "right"
+    stageRootLayer = appController._dataModel.stage.GetRootLayer()
+    stageRootLayer.TransferContent(Sdf.Layer.FindOrOpen("usd-12098/test.usda"))
+
+    # This image should contain just the other "left" and "right" cubes.
+    # The cubes under the "Over" and "PointInstancer" prims do not draw
+    # since they are considered prototypes (without any instances).
+    appController._takeShot("unprototyping-before.png")
+
+    # Turn the over to a def.  This should result in the prim underneath
+    # getting added.
+    stageRootLayer.GetPrimAtPath("/Over").specifier = Sdf.SpecifierDef
+
+    # Turn the prototype into a Scope.  Ensure it's children get imaged.
+    stageRootLayer.GetPrimAtPath("/PointInstancer").typeName = "Scope"
+
+    # This image should contain all 4 cubes.
+    appController._takeShot("unprototyping-after.png")
+
+
 
 def testUsdviewInputFunction(appController):
     _modifySettings(appController)
     _testInstancingEdits6146(appController)
+    _testMakePrototypeNotAPrototype(appController)
